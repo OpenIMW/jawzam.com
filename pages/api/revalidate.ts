9f9cc5fbc42db4006406b07210da2fd3d602cb23
Yassine Sedrani <cxmoro@gmail.com>
2023-03-01 16:08:08 +0000

    Override NextJS body parser

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,23 +1,31 @@
-import { isValidRequest } from "@sanity/webhook"
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook"
 import { NextApiRequest, NextApiResponse } from "next"
 
 const secret = process.env.SANITY_WEBHOOK_SECRET!
 
 export default async function revalidate(req: NextApiRequest, res: NextApiResponse) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+
   // Validate request method
   if (req.method !== 'POST') {
     return res.status(401).json({ message: "Must be a POST request" })
   }
 
-  // Validate signature
-  if (!isValidRequest(req, secret)) {
-    return res.status(401).json({ message: "Invalid signature" })
+  const body = await readBody(req) // Read the body into a string
+  if (!isValidSignature(body, signature, secret)) {
+    res.status(401).json({ success: false, message: 'Invalid signature' })
+    return
   }
+
+  // Validate signature
+  /* if (!isValidRequest(req, secret)) {
+    return res.status(401).json({ message: "Invalid signature" })
+  } */
 
   try {
     const {
       body: { type, slug },
-    } = req
+    } = JSON.parse(body)
 
     switch (type) {
       case "homePage":
@@ -31,6 +39,21 @@ export default async function revalidate(req: NextApiRequest, res: NextApiRespon
     }
 
   } catch (err) {
-    return res.status(500).send({ message: "Error revalidating" })
+    return res.status(500).send({ message: "Error revalidating", payload: body })
   }
+}
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+async function readBody(readable: NextApiRequest) {
+  const chunks = []
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+  }
+  return Buffer.concat(chunks).toString('utf8')
 }
